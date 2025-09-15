9aef4dad9ff688eade9fac30081b294cef832c29
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-09-15 16:14:29 -0300

    `Feat: Favoritos` | Listado de personajes favoritos.

--- src/app/(tabs)/favoritos.js
+++ src/app/(tabs)/favoritos.js
@@ -1,10 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { globalStyles } from "../../styles";
+import { useFavoritos } from "../../context/FavoritosContext";
+import Card from "../../components/Card";
+import { useRouter } from "expo-router";
 
 export default function Favoritos() {
+    const router = useRouter();
+    const { favoritos, dispatch } = useFavoritos();
+
+    const toggleFavorito = (item) => {
+        const isFav = favoritos.some(fav => fav.id === item.id);
+        dispatch({
+            type: isFav ? "REMOVE_FAVORITO" : "ADD_FAVORITO",
+            payload: item
+        });
+    };
+
+    if (favoritos.length === 0) {
+        return (
+            <View style={globalStyles.container}>
+                <Text style={globalStyles.title}> No hay personajes favoritos ;( </Text>
+            </View>
+        );
+    };
+
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.title}>Pantalla de Favoritos</Text>
+            <FlatList
+                data={favoritos}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <Card
+                        title={item.name}
+                        subtitle={item.status}
+                        image={item.image}
+                        onPress={() => router.push(`/personaje/${item.id}`)}
+                        onFavorite={() => toggleFavorito(item)} // función del contexto
+                        isFavorite={favoritos.some(fav => fav.id === item.id)} // chequeo
+                    />
+                )
+                }
+            />
         </View>
     );
 }
